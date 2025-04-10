5a4e3349d57c0dfcf1cbcd5124a59a9ccdd10741
GiorgioV-1 <114691570+GiorgioV-1@users.noreply.github.com>
2025-04-10 15:20:57 -0700

    Inital Push

---
+++ src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       201131602                                                    #
+# 	Created:      4/10/2025, 3:20:43 PM                                        #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
