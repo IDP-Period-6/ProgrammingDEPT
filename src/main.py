71bb57c3c702a38ba63dbe61e8ab7e921aada47e
GiorgioV-1 <114691570+GiorgioV-1@users.noreply.github.com>
2025-04-25 13:41:33 -0700

    add vision sensor code

--- src/main.py
+++ src/main.py
@@ -12,4 +12,20 @@ from vex import *
 
 # Brain should be defined by default
 brain=Brain()
-aivisionsensor = AiVision(Ports.PORT1)
+aivisionsensor = AiVision(Ports.PORT1, AiVision.ALL_TAGS)
+snapshot = aivisionsensor.take_snapshot(AiVision.ALL_TAGS)
+
+def imgthread():
+    # function will run in a separate thread so it can continue running in the background
+    while True:
+        global snapshot
+        snapshot = aivisionsensor.take_snapshot(AiVision.ALL_TAGS)
+        wait(50)
+thread = Thread(imgthread)
+
+while True:
+    for obj in snapshot:
+        brain.screen.clear_screen()
+        brain.screen.set_cursor(1, 1)
+        brain.screen.print("Tag detected: ", obj.id)
+        wait(0.5, SECONDS)
