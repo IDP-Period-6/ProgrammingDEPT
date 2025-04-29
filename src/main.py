2a5b73304d59e625ad29e76c95360d132b9026f5
WillyWally34 <willge2010@gmail.com>
2025-04-29 14:03:01 -0700

    Co-authored-by: GiorgioV-1 <GiorgioV-1@users.noreply.github.com>

--- src/main.py
+++ src/main.py
@@ -14,8 +14,60 @@ from vex import *
 brain=Brain()
 aivisionsensor = AiVision(Ports.PORT1, AiVision.ALL_TAGS)
 snapshot = aivisionsensor.take_snapshot(AiVision.ALL_TAGS)
-aiChecker = False
-distanceChecker = True
+
+# inertial sensor code
+# inertialSensor = Inertial(Ports.PORT10)
+# inertialSensor.calibrate()
+# while inertialSensor.is_calibrating():
+#     wait(100, MSEC)
+
+# distance sensor code
+distanceSensor = Distance(Ports.PORT19)
+value = distanceSensor.object_distance(INCHES)
+
+# drivetrain code
+leftMotor1 = Motor(Ports.PORT2, GearSetting.RATIO_18_1)
+leftMotor2 = Motor(Ports.PORT3, GearSetting.RATIO_18_1)
+rightMotor1 = Motor(Ports.PORT4, GearSetting.RATIO_18_1)
+rightMotor2 = Motor(Ports.PORT5, GearSetting.RATIO_18_1)
+leftMotors = MotorGroup(leftMotor1, leftMotor2)
+rightMotors = MotorGroup(rightMotor1, rightMotor2)
+# drivetrain = SmartDrive(leftMotors, rightMotors, Inertial, 319.19, 295, 40, MM) # change this
+
+#global variables for running
+aiChecker = True
+distanceChecker = False
+
+# actually used variables
+fowardCheck = False
+RobotFinished = False
+leftOrRight = 1
+startOrStop = False
+xCord = 6
+yCord = 0
+
+
+
+while startOrStop == True:
+    value = distanceSensor.object_distance(INCHES)
+    if (value <= 3): 
+        print("Drive Forward")
+    elif(value > 3):
+        print("Don't Drive Forward")
+        startOrStop = True		
+          
+def forwardIsClear():
+    if(value > 5):
+        forwardCheck = True
+    elif(value < 5):
+        forwardCheck = False
+
+while (RobotFinished == False and startOrStop == True):
+    forwardCheck = False
+    forwardIsClear()
+    if(forwardCheck == False):
+	    yCord += 1
+
 
 
 def imgthread():
@@ -28,11 +80,20 @@ thread = Thread(imgthread)
 
 while aiChecker == True:
     for obj in snapshot:
-        brain.screen.clear_screen()
         brain.screen.set_cursor(1, 1)
         brain.screen.print("Tag detected: ", obj.id)
         wait(0.5, SECONDS)
+        brain.screen.clear_screen()
 
-
-while distanceChecker == True:
-    
+while distanceChecker == False:
+    value = distanceSensor.object_distance(INCHES)
+    if value <= 3:
+        brain.screen.set_cursor(1,2)
+        print("DO NOT START")
+        wait(1, SECONDS)
+        brain.screen.clear_screen()
+    elif value > 3:
+        brain.screen.set_cursor(20,1)
+        print("START NOW!")
+        wait(1, SECONDS)
+        brain.screen.clear_screen()
