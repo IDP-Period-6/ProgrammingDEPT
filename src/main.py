622389fb337f8ebf6fc50573bba71da56225baec
WillyWally34 <willge2010@gmail.com>
2025-05-20 14:23:41 -0700

    edits

--- src/main.py
+++ src/main.py
@@ -24,6 +24,36 @@ drivetrain_l_needs_to_be_stopped_controller_1 = False
 drivetrain_r_needs_to_be_stopped_controller_1 = False
 
 # define a task that will handle monitoring inputs from controller_1
+
+# inertial sensor code
+# Allows us to get headings and turn accurately
+inertialSensor = Inertial(Ports.PORT4)
+inertialSensor.set_heading(0, DEGREES)
+inertialSensor.calibrate()
+while inertialSensor.is_calibrating():
+    wait(100, MSEC)
+# drivetrain code
+leftMotor1 = Motor(Ports.PORT11, GearSetting.RATIO_18_1,)
+leftMotor2 = Motor(Ports.PORT12, GearSetting.RATIO_18_1, )
+
+leftMotor1.set_reversed(True)
+leftMotor2.set_reversed(True)
+
+
+rightMotor1 = Motor(Ports.PORT1, GearSetting.RATIO_18_1,  )
+rightMotor2 = Motor(Ports.PORT2, GearSetting.RATIO_18_1,  )
+
+#claw heights controls are inversed
+clawHeight = Motor(Ports.PORT6, GearSetting.RATIO_18_1, False)
+clawControl = Motor(Ports.PORT15, GearSetting.RATIO_18_1)
+
+leftMotors = MotorGroup(leftMotor1, leftMotor2, )
+rightMotors = MotorGroup(rightMotor1, rightMotor2, )
+
+drivetrain = SmartDrive(leftMotors, rightMotors, inertialSensor)
+
+# define variable for remote controller enable/disable
+remote_control_code_enabled = True
 def rc_auto_loop_function_controller_1():
     global drivetrain_l_needs_to_be_stopped_controller_1, drivetrain_r_needs_to_be_stopped_controller_1, remote_control_code_enabled
     # process the controller input every 20 milliseconds
@@ -78,27 +108,23 @@ def rc_auto_loop_function_controller_1():
                 rightMotors.spin(FORWARD)
         # wait before repeating the process
         wait(20, MSEC)
-
-# define variable for remote controller enable/disable
-remote_control_code_enabled = True
-
 rc_auto_loop_thread_controller_1 = Thread(rc_auto_loop_function_controller_1)
 
 # ai vision sensor code
+# Allows us to get the purple color signature 
+#needed to insert the vial into the disemination chamber
 purple = Colordesc(1, 214, 72, 219, 10, 0.2)
 aivisionsensor = AiVision(Ports.PORT14, AiVision.ALL_TAGS, purple)
 snapshot = aivisionsensor.take_snapshot(AiVision.ALL_TAGS)
 
-# inertial sensor code
-inertialSensor = Inertial(Ports.PORT4)
-inertialSensor.set_heading(0, DEGREES)
-inertialSensor.calibrate()
-while inertialSensor.is_calibrating():
-    wait(100, MSEC)
+
 
 
 
 # distance sensor code
+# Allows us to get the distance an object is from
+#the distance sensor this is useful for when we have to position
+#ourselves accurately such as when we pick up the vial and drop it off
 distanceSensor = Distance(Ports.PORT5)
 value = distanceSensor.object_distance(INCHES)
 
@@ -108,78 +134,71 @@ rightValue = rightDistance.object_distance(INCHES)
 leftDistance = Distance(Ports.PORT13)
 leftValue = leftDistance.object_distance(INCHES)
 
-
-# drivetrain code
-leftMotor1 = Motor(Ports.PORT11, GearSetting.RATIO_18_1,)
-leftMotor2 = Motor(Ports.PORT12, GearSetting.RATIO_18_1, )
-
-leftMotor1.set_reversed(True)
-leftMotor2.set_reversed(True)
-
-
-rightMotor1 = Motor(Ports.PORT1, GearSetting.RATIO_18_1,  )
-rightMotor2 = Motor(Ports.PORT2, GearSetting.RATIO_18_1,  )
-
-
-clawHeight = Motor(Ports.PORT6, GearSetting.RATIO_18_1, False)
-clawControl = Motor(Ports.PORT15, GearSetting.RATIO_18_1)
-
-leftMotors = MotorGroup(leftMotor1, leftMotor2, )
-rightMotors = MotorGroup(rightMotor1, rightMotor2, )
-
-drivetrain = SmartDrive(leftMotors, rightMotors, inertialSensor)
 #endregion 
 
+        
 #global variables for running
 vialChecker = False
 distanceChecker = False
-tubeChecker = True
+tubeChecker = False
 start = False 
 
 # actually used variables
 forwardClear = False
 leftClear = False
 rightClear = False
-RobotFinished = True
+RobotFinished = False
 leftOrRight = 1
-start = True
+start = False
 
+#robot position coordinates
 xCord = 6 
 yCord = 0
 
+# This variable is set to true if space infront of the robot is clear
+# This variable allows us and the robot to know whether there
+#is enough safe space infront of the robot for it to move forward safely
 def forwardIsClear():
     global forwardClear
     value = distanceSensor.object_distance(INCHES)
-    if(value > 7):
+    if(value > 10):
         forwardClear = True
-    elif(value < 7):
+    elif(value < 10):
         forwardClear = False
-
+# This variable allows us and the robot to know whether there
+#is enough safe space to the lefto the left of the robot for it to move left safely
 def leftIsClear():
     global leftClear
     leftValue = leftDistance.object_distance(INCHES)
-    if (leftValue > 7):
+    if (leftValue > 10):
         leftClear = True
-    elif(leftValue < 7):
+    elif(leftValue < 10):
         leftClear = False
-
+# This variable allows us and the robot to know whether there
+#is enough safe space to the left of the robot for it to move right safely
 def rightIsClear():
     global rightClear
     rightValue = rightDistance.object_distance(INCHES)
-    if (rightValue > 7):
+    if (rightValue > 10):
         rightClear = True
-    elif(rightValue < 7):
+    elif(rightValue < 10):
         rightClear = False
 
+#keeps track of robot's current position on field using coordinates (x and y)
 def coordinate_tracker():
     global xCord
     global yCord
+    #robot moved up the field 1 in the y axis
     if inertialSensor.heading() <= 10 and inertialSensor.heading() >= 350: 
         yCord += 1
+    #robot moved to the left side of the field 1 in the x axis
     elif inertialSensor.heading() <= 80 and inertialSensor.heading() >= 100:
         xCord += 1
+    #robot moved down the field, -1 in the y axis
+    # 1 in the y axis
     elif inertialSensor.heading() <= 170 and inertialSensor.heading() >= 190:
         yCord -= 1
+    #robot moved to the right side of the field, -1 in 
     elif inertialSensor.heading() <= 260 and inertialSensor.heading() >= 280:
         xCord -= 1
     print(xCord)
@@ -187,13 +206,19 @@ def coordinate_tracker():
 
 drivetrain.set_drive_velocity(10, PERCENT)
 def vialDetection():
+    # This makes sure that when we are checking which vial to grab
+    #we take a picture of all of the availabe tags and sort through them
+    #in the next statement
     while vialChecker == True:
         aivisionsensor.tag_detection(True)
         snapshot = aivisionsensor.take_snapshot(AiVision.ALL_TAGS)
         value = distanceSensor.object_distance(MM)
         print("hello")
         if(value < 477):
+            #This is a for loo used to check every single april tag
+            #that was taken inside the picture for the vials
             for obj in snapshot:
+                #This will print to the scree
                 print("Tag detected: ", obj.id)
                 if obj.id == 9:
                     drivetrain.drive_for(FORWARD, 2.5, INCHES)
@@ -211,13 +236,10 @@ def vialDetection():
         
         
 # set this as the height of the claw to stay at
-clawControl.spin_for(FORWARD, 60, DEGREES)
-
-clawHeight.spin_for(FORWARD, 255, DEGREES)
+#clawHeight.spin_for(FORWARD, 255, DEGREES)
 # this is the vial wideness
-# clawControl.spin_for(REVERSE, 55, DEGREES)
-# wait(1, SECONDS)
-# vialDetection()
+#wait(1, SECONDS)
+#vialDetection()
 
 
 
@@ -225,9 +247,10 @@ clawHeight.spin_for(FORWARD, 255, DEGREES)
 # 1 checks when to start the entire autonomous routine
 while start == False:
     value = distanceSensor.object_distance(INCHES)
-    if (value < 12): 
+    clawHeight.spin_for(FORWARD, 240, DEGREES)
+    if (value < 10): 
         print("Don't Drive Forward")
-    elif(value > 12):
+    elif(value > 10):
         print("Drive Forward")
         start = True		
 
@@ -236,7 +259,7 @@ while(RobotFinished == False and start == True):
     # drivetrain drive forward for one y coordinate 
     forwardIsClear()
     drivetrain.set_drive_velocity(20, PERCENT)
-    if (yCord == 6 and xCord == 8):
+    if (yCord == 20 and xCord == 4):
         # if the coordinates of the robot are perfectly in front of the vials
         # then we can stop the robot
         drivetrain.stop(BRAKE)
@@ -316,26 +339,10 @@ while(RobotFinished == False and start == True):
         print("clear")
         wait(1, SECONDS)
         forwardClear = True
-        drivetrain.drive_for(6, INCHES)
+        drivetrain.drive_for(FORWARD, 6, INCHES)
         coordinate_tracker()
         
 
-                
-
-
-
-while distanceChecker == True:
-    value = distanceSensor.object_distance(INCHES)
-    if value <= 3:
-        brain.screen.set_cursor(1,2)
-        print("DO NOT START")
-        wait(1, SECONDS)
-        brain.screen.clear_screen()
-    elif value > 3:
-        brain.screen.set_cursor(20,1)
-        print("START NOW!")
-        wait(1, SECONDS)
-        brain.screen.clear_screen()
 
 #Should work needs testing
 def waitForLever():
@@ -352,22 +359,38 @@ def waitForLever():
 
 
 
-
+#function for dropping the vial in the
 def dropOff():
+    # we use the start_awb in order to clear the whiteness in the ai vision sensor 
+    # this balances the color and prevents extra yellowing
+    aivisionsensor.start_awb()
+    # this entire while loop inside of the drop off function
+    # only starts when the variable is set true so it doesn't function
+    # until the variable is set true
     while tubeChecker == True:
+        aivisionsensor.color_detection(True)
         tubeColor = aivisionsensor.take_snapshot(purple)
         value = distanceSensor.object_distance(MM)
-        if (value < 427):
+        print(value)
+        if (value < 600 and value > 590):
+            drivetrain.stop(BRAKE)
             if len(tubeColor) >= 1:
                 print("tube detected")
-                clawHeight.spin_for(FORWARD, 140, DEGREES)
-                drivetrain.drive_for(FORWARD, 4, INCHES)
+                clawHeight.spin_for(FORWARD, 160, DEGREES)
+                drivetrain.drive_for(FORWARD, 6.25, INCHES)
                 wait(1, SECONDS)
                 clawHeight.set_velocity(10, PERCENT)
                 clawHeight.spin_for(REVERSE, 60, DEGREES)
                 wait(1, SECONDS)
                 clawControl.spin_for(REVERSE, 60, DEGREES)
                 drivetrain.drive_for(REVERSE, 3, INCHES)
-        elif(value > 428):
-            drivetrain.drive_for(FORWARD, 1, MM)
+        elif(value > 600):
+            drivetrain.drive(FORWARD)
+
+
+'''''
+clawControl.spin_for(FORWARD, 75, DEGREES)
+clawHeight.spin_for(FORWARD, 240, DEGREES)
+drivetrain.set_heading(0, DEGREES)
 dropOff()
+'''
